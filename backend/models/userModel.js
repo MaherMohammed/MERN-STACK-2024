4bb77492c4249263f238651bb88a1a8503a051ca
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-04-30 19:44:59 +0300

    hash password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -19,6 +19,17 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+
+// add middleware to hash password
+userSchema.pre('save', async function(next){
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    // create the key that used to hash the password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User',userSchema)
 
