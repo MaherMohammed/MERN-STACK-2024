d5e653c20133a44b65d960afbb8aee983d3c2c2d
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-04-30 19:10:33 +0300

    connectDB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,52 @@
 import asyncHandler from 'express-async-handler'
 
+
+// Auth user/ set token
+// route POST /api/users/auth
+
 const authUser = asyncHandler (async(req,res) =>{
+
     res.status(200).json({message: "Auth User"})
 })
 
+
+// Register new user
+// route POST /api/users
+const registerUser = asyncHandler (async(req,res) =>{
+
+    res.status(200).json({message: "Register User"})
+})
+
+
+// logout user
+// route POST /api/users/logout
+const logoutUser = asyncHandler (async(req,res) =>{
+
+    res.status(200).json({message: "Logout User"})
+})
+
+
+// get user profile
+// route GET /api/users/profile
+const getUserProfile = asyncHandler (async(req,res) =>{
+
+    res.status(200).json({message: "User Profile"})
+})
+
+
+
+// update user profile
+// route PUT /api/users/profile
+const updateUserProfile = asyncHandler (async(req,res) =>{
+
+    res.status(200).json({message: "Update User Profile"})
+})
+
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errormiddleware.js'; 
+import connectDB from './config/db.js'
+
 
 dotenv.config()
 const PORT = process.env.PORT || 5000; 
+
+connectDB()
 
 const app = express();
 
@@ -15,6 +20,10 @@ app.use('/api/users',userRoutes)
 app.get('/',(req,res)=>{
     res.send('Server is ready')
 })
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 
 app.listen(PORT, ()=>{
