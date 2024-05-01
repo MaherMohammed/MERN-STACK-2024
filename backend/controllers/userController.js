688ef2e6ba7c2181ee9d73030017675bf6da6d0a
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-05-01 14:27:48 +0300

    get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,8 +77,12 @@ const logoutUser = asyncHandler (async(req,res) =>{
 // get user profile
 // route GET /api/users/profile
 const getUserProfile = asyncHandler (async(req,res) =>{
-
-    res.status(200).json({message: "User Profile"})
+    const user = {
+        _id: req.user._id,
+        name:req.user.name,
+        email: req.user.email
+    }
+    res.status(200).json({user})
 })
 
 
