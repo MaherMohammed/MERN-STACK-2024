47d540ff06091fc759e1121a3042e43c3357a142
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-05-01 14:36:01 +0300

    update profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -90,7 +90,29 @@ const getUserProfile = asyncHandler (async(req,res) =>{
 // update user profile
 // route PUT /api/users/profile
 const updateUserProfile = asyncHandler (async(req,res) =>{
+    const user = await User.findById(req.user._id)
 
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email
+        })
+
+    }
+    else{
+        res.status(404);
+        throw new Error('User Not Found!')
+    }
     res.status(200).json({message: "Update User Profile"})
 })
 
